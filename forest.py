cdac11d66b66e9dfeeba4676e3e9e54f64f50720
alazharn <alazhar.naji@gmail.com>
2016-12-29 01:10:30 +0100

    Update forest.py
    Add testing set which doesn't intersect the training set

--- forest.py
+++ forest.py
@@ -18,6 +18,13 @@ data_train = np.asarray(list_train)
 data_train = np.concatenate(data_train)
 Y_train = data_train[:, forest_type_index]
 X_train = data_train[:, 0:forest_type_index]
+list_test = [list_by_forest_types[i][int(train_ratio*list_by_forest_types[i].size/line_size):int(list_by_forest_types[i].size/line_size)] for i in range(0,7)]
+data_test = np.asarray(list_test)
+data_test = np.concatenate(data_test)
+Y_test = data_test[:,forest_type_index]
+X_test = data_test[:,0:forest_type_index]
+
+Y_test = np_utils.to_categorical(np.add(Y_test,-1), 7)
 
 model = Sequential()
 model.add(Dense(dim, input_dim=dim))
